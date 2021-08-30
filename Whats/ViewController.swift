3ddec1a927f7b07b2a6e19f6c2ae0400c1faf001
uladzimir.draniuk <uladzimir.draniuk@gmail.com>
2021-08-31 00:48:31 +0300

    step_!

--- Whats/ViewController.swift
+++ Whats/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var x = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
