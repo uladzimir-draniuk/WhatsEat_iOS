9e1b1a1fbded6f2f1342bcd199e98f42a9e155d0
uladzimir.draniuk <uladzimir.draniuk@gmail.com>
2021-08-31 00:45:28 +0300

    Initial Commit

---
+++ Whats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whats
+//
+//  Created by elf on 31.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
